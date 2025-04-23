6096dba602f8b07ed97a0a62bede21546a7826fb
Matt Wittmann <mcwitt@gmail.com>
2025-04-23 12:14:07 -0700

    Add comments

--- ising2d.cu
+++ ising2d.cu
@@ -35,9 +35,20 @@ __host__ __device__ inline auto ceil_div(unsigned int x, unsigned int y)
 }
 
 __device__ void k_reduce_sum(int &val, int *out) {
+
+  /* Computes the sum of val for all threads in a block and stores the
+    result in out. */
+
+  // 1. Compute sum of values in each warp
+
   for (int offset = warpSize / 2; offset > 0; offset /= 2) {
     val += __shfl_down_sync(0xFFFFFFFF, val, offset);
   }
+
+  // At this point, the first thread in each warp ("warp leader") has
+  // for its value the sum of the values over the warp.
+
+  // 2. Warp leaders store warp sums in shared memory
 
   __shared__ int warp_sums[32];
 
@@ -47,17 +58,21 @@ __device__ void k_reduce_sum(int &val, int *out) {
     warp_sums[tid / warpSize] = val;
   }
 
-  __syncthreads();
+  // 3. Threads in first warp reduce warp sums.
+
+  __syncthreads(); // ensure all threads see the final value of warp_sums
 
   const unsigned int tpb = blockDim.x * blockDim.y;
+  const unsigned int nwarps = ceil_div(tpb, warpSize);
 
   if (tid < warpSize) {
-    val = (tid < ceil_div(tpb, warpSize)) ? warp_sums[tid] : 0;
+    val = (tid < nwarps) ? warp_sums[tid] : 0;
 
     for (int offset = warpSize / 2; offset > 0; offset /= 2) {
       val += __shfl_down_sync(0xFFFFFFFF, val, offset);
     }
 
+    // First warp leader updates out
     if (tid == 0) {
       atomicAdd(out, val);
     }
