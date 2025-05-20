043909c526ecbba2a6407e3c18c9df38b5538a9a
Matt Wittmann <mcwitt@gmail.com>
2025-05-20 15:04:08 -0700

    Add notebooks

---
+++ python/notebooks/flake.nix
@@ -0,0 +1,62 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs.nixgl.url = "github:nix-community/nixGL";
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixgl,
+    }:
+    let
+      system = "x86_64-linux";
+
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+          cudaSupport = true;
+        };
+        overlays =
+          let
+            overlay = self: super: {
+              pythonPackagesExtensions = (super.pythonPackagesExtensions or [ ]) ++ [
+                (pySelf: pySuper: {
+                  ising-mcmc = pySelf.callPackage ../package.nix { };
+                })
+              ];
+            };
+          in
+          [
+            nixgl.overlays.default
+            overlay
+          ];
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages =
+          let
+            python = pkgs.python3.withPackages (
+              ps: with ps; [
+                ipywidgets
+                ising-mcmc
+                jupytext
+                notebook
+                seaborn
+                tqdm
+              ]
+            );
+          in
+          [
+            python
+            pkgs.basedpyright
+          ]
+          ++ nixpkgs.lib.optional (builtins ? currentTime) pkgs.nixgl.auto.nixGLDefault;
+
+        shellHook = ''
+          export PYTHONPATH=build/:$PYTHONPATH
+        '';
+      };
+    };
+}
---
+++ python/notebooks/main.py
@@ -0,0 +1,59 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.2
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+# %config InlineBackend.figure_format = "retina"
+
+# %%
+import matplotlib.pyplot as plt
+
+# %%
+import numpy as np
+
+from ising_mcmc import cuda
+
+# %%
+rng = np.random.default_rng(0)
+
+# %%
+l = 1024
+
+# %%
+tc = 2.269
+temps = tc * np.array([0.9, 1.0, 1.1])
+nt = len(temps)
+
+# %%
+spin = 2 * rng.binomial(1, 0.5, size=(nt, l, l)) - 1
+hext = np.zeros((nt, l, l), dtype=np.float32)
+
+# %%
+spin.shape
+
+# %%
+_, axs = plt.subplots(1, 3, figsize=(10, 3))
+for s_t, ax in zip(spin, axs):
+    ax.imshow(s_t)
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+# %%
+spin_, accept_rate, m2avg, m4avg = cuda.sweeps(spin, hext, temps, 10_000, 0)
+
+# %%
+_, axs = plt.subplots(1, 3, figsize=(10, 3))
+for s_t, ax in zip(spin_, axs):
+    ax.imshow(s_t)
+    ax.set_xticks([])
+    ax.set_yticks([])
