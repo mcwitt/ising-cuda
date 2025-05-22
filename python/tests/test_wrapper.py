c0e3c6ea77118e1b10d8d81fd05edbd50558f10c
Matt Wittmann <mcwitt@gmail.com>
2025-05-22 16:14:10 -0700

    Specialize 2d kernel

--- python/tests/test_wrapper.py
+++ python/tests/test_wrapper.py
@@ -4,27 +4,28 @@ import pytest
 from ising_mcmc.cuda import sweeps
 
 
-def test_sweeps():
-    l = 64
-    nt = 16
-
+@pytest.mark.parametrize("nt", [1, 8])
+@pytest.mark.parametrize("d,l", [(1, 1), (1, 64), (2, 2), (2, 16), (3, 4), (10, 2)])
+def test_sweeps(d, l, nt):
     rng = np.random.default_rng(0)
-    spin = 2 * rng.binomial(1, 0.5, size=(nt, l, l)) - 1
-    hext = np.zeros((nt, l, l)).astype(np.float32)
-    temps = np.linspace(2.0, 3.0, nt, dtype=np.float32)
+    shape = (nt,) + (l,) * d
+    spin = 2 * rng.binomial(1, 0.5, size=shape) - 1
+    hext = np.zeros(shape).astype(np.float32)
+    temps = np.linspace(2.20, 2.34, nt, dtype=np.float32)
 
-    result_tuple = sweeps(spin, hext, temps, 100, 0)
+    result_tuple = sweeps(spin, hext, temps, 300, 0)
     assert isinstance(result_tuple, tuple)
     assert len(result_tuple) == 4
     spin_, accept_rate, m2a, m4a = result_tuple
 
-    assert spin_.shape == (nt, l, l)
-    assert set(spin_.ravel().tolist()) == {-1, 1}
+    assert spin_.shape == shape
+    assert set(spin_.ravel().tolist()) <= {-1, 1}
 
     def check_values(values):
         assert isinstance(values, np.ndarray)
         assert values.shape == (len(temps),)
-        np.testing.assert_array_less(0.0, values)
+        assert np.all(0.0 <= values)
+        assert np.any(0.0 < values)
 
     check_values(accept_rate)
     check_values(m2a)
@@ -53,3 +54,8 @@ def test_sweeps_validation():
     spin[1, 1, 1] = 0
     with pytest.raises(ValueError, match="invalid value in spin: 0"):
         sweeps(spin, np.ones((8, 8, 8)), np.ones((8,)), 100, 0)
+
+    with pytest.raises(
+        ValueError, match="number of dimensions must be between 1 and 10, but got 11"
+    ):
+        sweeps(np.ones((2,) * 12), np.ones((2,) * 12), np.ones((2,)), 100, 0)
